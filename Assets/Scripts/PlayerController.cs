88d32aac4a9d49e980369b8f3dd46ed3361ef6c7
Petrescu Viorel Stefan <64736176+stefan-vhub@users.noreply.github.com>
2022-07-15 07:41:00 +0300

    15.07.2022

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D theRB;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+        theRB.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), theRB.velocity.y);
+    }
+}
